e7161fadc9081077f437bb66824c4ec0c04615d3
minlucyzhao <minlucyzhao@gmail.com>
2019-04-13 02:07:44 +0000

    User Registration

--- express_server.js
+++ express_server.js
@@ -25,7 +25,18 @@ var urlDatabase = {
 };
 
 //Global Object "users"
-const users = {};
+const users = { 
+    "userRandomID": {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    },
+   "user2RandomID": {
+      id: "user2RandomID", 
+      email: "user2@example.com", 
+      password: "dishwasher-funk"
+    }
+  }
 //---------------------------------------------------------------
 
 
@@ -49,28 +60,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 //---------------------------------------------------------------
 app.get("/urls", (req, res) => {
     //let cookie_id = res.cookie("Cookies ", res.cookie).userID
+    console.log("users:", users);
+    console.log(req.cookies["user_id"]);
     let templateVars = {
       user_id: req.cookies["user_id"], 
       urls: urlDatabase,
-      email: users[req.cookies["user_id"]].email
+      email: users[req.cookies["user_id"]]["email"]
     }; 
-  res.render("urls_index", templateVars);
+    console.log(templateVars);
+    res.render("urls_index", templateVars);
 });
-//---------------------------------------------------------------
-
-
-//---------------------------------------------------------------
-//PAGE FOR EACH SHORTURL
-//http://localhost:8080/urls/:shortURL
-//---------------------------------------------------------------
-app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = {
-        user_id: req.cookies["user_id"], 
-        shortURL: req.params.shortURL, 
-        longURL: urlDatabase[req.params.shortURL] 
-    };
-    res.render("urls_show", templateVars);
-  });
 //---------------------------------------------------------------
 
 
@@ -83,7 +82,9 @@ app.get("/urls/:shortURL", (req, res) => {
 //Rule of Thumb: Routes should be ordered from most specific to least specific
 app.get("/urls/new", (req, res) => {
     let templateVars = {
-        user_id: req.cookies["user_id"]}
+        user_id: req.cookies["user_id"],
+        email: users[req.cookies["user_id"]].email
+    };
     res.render("urls_new", templateVars);
   });
 
@@ -97,11 +98,34 @@ app.post("/urls", (req, res) => {
 
 
 //---------------------------------------------------------------
+//PAGE FOR EACH SHORTURL
+//http://localhost:8080/urls/:shortURL
+//---------------------------------------------------------------
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = {
+        user_id: req.cookies["user_id"], 
+        shortURL: req.params.shortURL, 
+        longURL: urlDatabase[req.params.shortURL],
+        email: users[req.cookies["user_id"]].email
+    };
+    res.render("urls_show", templateVars);
+  });
+//---------------------------------------------------------------
+
+
+//---------------------------------------------------------------
 //USER REGISTER
 //from registration.ejs
 //---------------------------------------------------------------
 app.get("/register", (req, res) => {
-    res.render("registration");
+    console.log(req.cookies["user_id"]);
+    console.log(users);
+    let templateVars = {};
+    if (req.cookies["user_id"] === undefined) {
+        res.render("registration", {user_id: undefined});
+    } else {
+        res.redirect("/urls");
+    }
 });
 
 app.post("/register", (req, res) => {
@@ -130,10 +154,10 @@ app.post("/register", (req, res) => {
 
 
 //---------------------------------------------------------------
-//EMAIL LOOKUP
-//helper function
+//EMAIL LOOKUP IMPORTANT (Functions should start with verb)
+//helper function 
 //---------------------------------------------------------------
-function emailLookup(userEmail) {
+function emailLookup(userEmail, users) {
     for (user in users) {
         if (userEmail === users[user]["email"]) {
             return true;
@@ -145,15 +169,50 @@ function emailLookup(userEmail) {
 
 
 //---------------------------------------------------------------
+//PASSWORD LOOKUP IMPORTANT (Functions should start with verb)
+//helper function
+//---------------------------------------------------------------
+function passwordLookup(userPassword, users) {
+    for (user in users) {
+        if (userPassword === users[user]["password"]) {
+            return true;
+        }
+    }
+    return false;
+}
+//---------------------------------------------------------------
+
+
+//---------------------------------------------------------------
 //LOGIN
 //---------------------------------------------------------------
-app.get ("/login", (req,res) => {
-    res.render("/login");
+app.get("/login", (req,res) => {
+    let templateVars = {};
+    if (req.cookies["user_id"] === undefined) {
+        templateVars = {
+            user_id: req.cookies["user_id"], 
+        }; 
+        res.render("login", templateVars);
+    } else {
+        res.redirect("/urls");
+    }
 })
 
 app.post("/login", (req,res) => {
-    res.cookie("user_id", req.body.userID);
-    res.redirect("/urls");
+    //check userDatabase if email and password exist
+    let userEmail = req.body.email;
+    let userPassword = req.body.password;
+    let userId = findUserId(userEmail, userPassword);
+    console.log("before", userId);
+
+    if(userId === "") {
+        console.log("entered",userId);
+        res.status(403);
+        res.send("Email and/or Password cannot be found.");
+    } else {
+        res.cookie("user_id", userId);
+        res.redirect("/urls");
+    }
 });
 //---------------------------------------------------------------
 
@@ -161,9 +220,14 @@ app.post("/login", (req,res) => {
 //---------------------------------------------------------------
 //LOGOUT
 //---------------------------------------------------------------
+app.get("/logout", (req,res) => {
+    res.clearCookie("user_id");
+    res.redirect("login");
+});
+
 app.post("/logout", (req,res) => {
     res.clearCookie("user_id");
-    res.redirect("/urls");
+    res.redirect("/login");
 });
 //---------------------------------------------------------------
 
@@ -194,7 +258,25 @@ app.post("/urls/:shortURL/update", (req,res) => {
 
 
 //---------------------------------------------------------------
-//DISPLAY urlDatabase ARRAY OF OBJECTS
+//FIND USER_ID WITH USER EMAIL
+//helper function
+//---------------------------------------------------------------
+function findUserId(userEmail, userPassword) {
+
+    let user_id = "";
+    for(user in users) {
+        if (users[user]["email"] === userEmail && users[user]["password"] === userPassword) {
+            user_id = users[user]["id"];
+            break;
+        }
+    }
+    return user_id;
+}
+//---------------------------------------------------------------
+
+
+//---------------------------------------------------------------
+//DISPLAY urlD atabase ARRAY OF OBJECTS
 //helper function
 //---------------------------------------------------------------
 //The "u" is to differentiate itself from /url/ and so it doesn't conflict with the other GET routes
